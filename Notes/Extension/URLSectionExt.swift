c979a710660feb86e9eaab2f938e4bb8a387ce9d
Fredy Camas <fredycamasdev@gmail.com>
2023-01-06 14:32:00 -0500

    added url extension

---
+++ Notes/Extension/URLSectionExt.swift
@@ -0,0 +1,28 @@
+//
+//  URLSectionExt.swift
+//  Notes
+//
+//  Created by Fredy Camas on 01/06/23.
+//
+
+import Foundation
+
+/// extension used to fetch data using generic data type
+extension URLSession {
+  func fetchData<T: Decodable>(for url: URL, completion: @escaping (Result<T, Error>) -> Void) {
+    self.dataTask(with: url) { (data, response, error) in
+      if let error = error {
+        completion(.failure(error))
+      }
+
+      if let data = data {
+        do {
+          let object = try JSONDecoder().decode(T.self, from: data)
+          completion(.success(object))
+        } catch let decoderError {
+          completion(.failure(decoderError))
+        }
+      }
+    }.resume()
+  }
+}
