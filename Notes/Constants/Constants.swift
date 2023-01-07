0f431c143b8efee3f0687818ee0bcbc2714ce587
Fredy Camas <fredycamasdev@gmail.com>
2023-01-07 13:43:00 -0500

    added string constant for url

---
+++ Notes/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Notes
+//
+//  Created by Fredy Camas on 01/07/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct DessertViewConstants{
+    let dessertImageURL = "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg"
+}
