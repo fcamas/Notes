9da10508a2c95d29b10c87ae89668cbd56dde565
Fredy Camas <fredycamasdev@gmail.com>
2023-01-08 14:55:00 -0500

    added string constant for main card

--- Notes/Constants/Constants.swift
+++ Notes/Constants/Constants.swift
@@ -11,3 +11,11 @@ import SwiftUI
 struct DessertViewConstants{
     let dessertImageURL = "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg"
 }
+
+struct DessertCardSizes{
+    var height = UIScreen.main.bounds.height*0.25
+    var width = UIScreen.main.bounds.width*9
+    var corneRadius:CGFloat = 20
+    var shadow:CGFloat = 10
+    var leading: CGFloat = 20
+}
