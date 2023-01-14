13faa0db8b91b4bd6ad462daf84aec5fc44055f2
Fredy Camas <fredycamasdev@gmail.com>
2023-01-14 14:43:00 -0500

    added dessert model

--- Notes/Model/DessertModel.swift
+++ Notes/Model/DessertModel.swift
@@ -7,6 +7,32 @@
 
 import Foundation
 
+import Foundation
+
+struct DessertModel {
+
+    struct Card: Decodable,Identifiable,Equatable {
+        var id:String
+        var imageURL: String
+        var name: String
+    }
+    
+    struct Detail: Identifiable {
+        var id:String
+        var imageURL: String
+        var isExpanded =  false
+        var mealName:String
+        var instructions:String
+        var measures: [Measures]
+    }
+
+    struct Measures:Identifiable {
+        var id =  UUID()
+        var ingredients:String
+        var measures:String
+    }
+}
+
 struct Dessert:Decodable{
     var meals: [Meals]
 }
