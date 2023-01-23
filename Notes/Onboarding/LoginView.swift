d185cd8beb83d427a05bd4dc9cb9efbae2b149da
Fredy Camas <fredycamasdev@gmail.com>
2023-01-23 14:55:00 -0500

    added login view

---
+++ Notes/Onboarding/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Notes
+//
+//  Created by Fredy Camas on 01/23/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
