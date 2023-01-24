4921e5167956ccccf0b92e741e493e70715acab8
Fredy Camas <fredycamasdev@gmail.com>
2023-01-24 14:13:00 -0500

    added login view

--- Notes/Onboarding/LoginView.swift
+++ Notes/Onboarding/LoginView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var text: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         Image("fiestaLogo")
             .resizable()
             .frame(width: 100, height: 120)
+        
+        VStack(alignment: .leading){
+            Text("Enter username")
+                .offset(x:0, y: 8)
+            
+            TextField("", text: $text).font(.system(size: 30))
+  
+        }.padding(.all, 32)
     }
 }
 
