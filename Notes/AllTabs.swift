58a86eb6f757f1e5b62953c0169af4d6f2dc0dda
Fredy Camas <fredycamasdev@gmail.com>
2023-02-04 14:15:00 -0500

    added tabs

---
+++ Notes/AllTabs.swift
@@ -0,0 +1,32 @@
+//
+//  AllTabs.swift
+//  Notes
+//
+//  Created by Fredy Camas on 02/04/23.
+//
+
+import SwiftUI
+
+//struct HomeView: View {
+//    var body: some View {
+//        Text("Home")
+//    }
+//}
+
+struct SearchView2: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct FavoritesView: View {
+    var body: some View {
+        Text("Favorites")
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
