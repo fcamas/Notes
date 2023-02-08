9906652c8a91a08b26637b02e4e285d12970e450
Fredy Camas <fredycamasdev@gmail.com>
2023-02-08 14:13:00 -0500

    added QRcode

--- Notes/ReservationView.swift
+++ Notes/ReservationView.swift
@@ -26,6 +26,12 @@ struct ReservationView: View {
     
     func generateQRCode(from string: String) -> UIImage {
         filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
         return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
         
